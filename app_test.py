782d0e67863609f93ed90029c3e2039c90ed8bf0
jincheol5 <oscar4293@gmail.com>
2025-01-15 12:27:51 +0900

    update

--- app_test.py
+++ app_test.py
@@ -20,5 +20,19 @@ torch.backends.cudnn.benchmark=False
 test all source
     node: 50
 """
-test_graph_50_list_dict=Data_Loader.load_from_pickle(file_name="test_graph_50_list_dict")
-Model_Trainer.evaluate(model_file_name="NGAE_MPNN_MAX_BFS_20",graph_list_dict=test_graph_50_list_dict,model_type="mpnn_max",latent_dim=32)
+# test_graph_list_dict=Data_Loader.load_from_pickle(file_name="test_graph_50_list_dict")
+# Model_Trainer.evaluate(model_file_name="NGAE_MPNN_MAX_BFS_20",graph_list_dict=test_graph_list_dict,model_type="mpnn_max",latent_dim=32)
+
+"""
+test all source
+    node: 100
+"""
+# test_graph_list_dict=Data_Loader.load_from_pickle(file_name="test_graph_100_list_dict")
+# Model_Trainer.evaluate(model_file_name="NGAE_MPNN_MAX_BFS_20",graph_list_dict=test_graph_list_dict,model_type="mpnn_max",latent_dim=32)
+
+"""
+test all source
+    node: 1000
+"""
+test_graph_list_dict=Data_Loader.load_from_pickle(file_name="test_graph_1000_list_dict")
+Model_Trainer.evaluate(model_file_name="NGAE_MPNN_MAX_BFS_20",graph_list_dict=test_graph_list_dict,model_type="mpnn_max",latent_dim=32)
